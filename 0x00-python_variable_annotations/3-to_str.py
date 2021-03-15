623798ae515da5351c89953e84693e489d09f132
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2021-03-15 23:49:57 +0100

    returns the string representation of the float

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -2,6 +2,6 @@
 """ returns the string representation of the float """
 
 
-def to_str(i: float) -> str:
+def to_str(n: float) -> str:
     """ returns the string representation of the float """
-    return str(i)
+    return str(n)

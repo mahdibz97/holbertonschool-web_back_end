f3e462fe49d4040d961326d3f7d0196dd957d58a
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2021-03-16 00:12:25 +0100

    tupl args

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" tupl args """
+
+
+from typing import Union, Tuple
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ tupl args """
+    return (k, v ** 2)

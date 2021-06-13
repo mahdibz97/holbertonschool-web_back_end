d1c45c58f244de7a811e75fdafa6c05a24fc2e94
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2021-06-13 23:00:26 +0100

    fix

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,20 +1,98 @@
 #!/usr/bin/env python3
-""" unittests to test if functions are working as expected """
+""" client unittesting Module """
+
+
 from client import GithubOrgClient
-from parameterized import parameterized
 import unittest
-from unittest.mock import patch
-from utils import get_json
+from unittest.mock import patch, PropertyMock
+from parameterized import parameterized, parameterized_class
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ class of unittests methods """
+    """ GithubOrgClient unittesting class """
+
     @parameterized.expand([
-       ("google"),
-       ("abc")
+        ("google"),
+        ("abc")
     ])
-    # @patch('utils.requests.get')
-    def test_org(self, org):
-        """ test_org method to test """
-        g = GithubOrgClient(org)
-        self.assertEqual(g.org(), "https://api.github.com/orgs/{}".format(org))
+    @patch('client.get_json', return_value={})
+    def test_org(self, org, patched):
+        """ GithubOrgClient.org unittesting """
+
+        test_class = GithubOrgClient(org)
+        self.assertEqual(test_class.org, patched.return_value)
+        patched.assert_called_once()
+
+    def test_public_repos_url(self):
+        """ GithubOrgClient._public_repos_url unittesting """
+
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock,
+                   return_value={'repos_url': 'http://bigBrain.url'}
+                   ) as patched:
+            test_class = GithubOrgClient("BigBrain")
+            result = test_class._public_repos_url
+            self.assertEqual(result, patched.return_value['repos_url'])
+
+    @patch('client.get_json',
+           return_value={'repos_url': 'http://bigBrain.url'})
+    def test_public_repos(self, first_patch):
+        """ GithubOrgClient.public_repos unittesting """
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock,
+                   return_value=[]
+                   ) as second_patch:
+            test_class = GithubOrgClient("BigBrain")
+            result = test_class.public_repos(license="SmallBrain")
+            self.assertEqual(result, second_patch.return_value)
+            first_patch.assert_called_once()
+            second_patch.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license, has_license):
+        """ GithubOrgClient.has_license unittesting """
+
+        test_class = GithubOrgClient("BigBrain")
+        self.assertEqual(test_class.has_license(repo, license),
+                         has_license)
+
+
+@parameterized_class(
+    ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ GithubOrgClient integration testing class """
+
+    @classmethod
+    def setUpClass(cls):
+        """ GithubOrgClient integration testing patcher setup """
+        cls.get_patcher = patch('requests.get')
+        cls.mock = cls.get_patcher.start()
+        cls.mock.return_value.json.side_effect = [
+            cls.org_payload, cls.repos_payload,
+            cls.org_payload, cls.repos_payload,
+        ]
+
+    @classmethod
+    def tearDownClass(cls):
+        """ GithubOrgClient integration testing patcher destroy """
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """ GithubOrgClient integration testing public_repos """
+        test_class = GithubOrgClient("BigBrain")
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("random"), [])
+
+    def test_public_repos_with_license(self):
+        """ GithubOrgClient integration testing public_repos with
+            a valid licence"""
+        test_class = GithubOrgClient("BigBrain")
+        self.assertEqual(test_class.public_repos(license="apache-2.0"),
+                         self.apache2_repos)

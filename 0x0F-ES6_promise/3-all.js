ee2160e2fc3d5b90fb574f03527f042ef0f39cd1
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2021-06-16 00:31:39 +0100

    fix

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,5 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((obj) => console.log(`${obj[0].body} ${obj[1].firstName} ${obj[1].lastName}`)).catch(() => console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
